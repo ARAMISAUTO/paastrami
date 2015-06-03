6c091af33c1cb21b5ea864878bc06154ecf01fe8
Tristan Rivoallan <tristan.rivoallan@aramisauto.com>
2015-06-03 14:18:45 +0200

    [env:init] on copie le dépôt Git

--- src/src/AramisAuto/Paastrami/Entity/Environment.php
+++ src/src/AramisAuto/Paastrami/Entity/Environment.php
@@ -42,7 +42,7 @@ class Environment
         $fs->mirror(
             $this->getPlatform()->getRepository(),
             $this->getDirectory(),
-            $finder->notName('.git')->followLinks()->in($this->getPlatform()->getRepository()),
+            $finder->followLinks()->in($this->getPlatform()->getRepository()),
             array('delete' => false)
         );
     }
